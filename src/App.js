91ec55a0c867be2e949a27c9ef474109c583fac7
Alvin Jiang <alvinjg@umich.edu>
2023-05-05 18:08:44 -0400

    empty login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+const App = (props) => {
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div class="navigation-bar">
+
+      </div>
+
+      <div class="login-form">
+        <input type="text" placeholder="Enter Username" required></input>
+        <input type="text" placeholder="Enter Password" required></input>
+
+        <button type="submit">Login</button>
+
+
+      </div>
+    </>
+
+  )
 }
 
-export default App;
+export default App
