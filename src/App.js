8c3b903abd8a2dfd8401a77539a1116f8f4537ee
fzheng04 <feiyang91604@gmail.com>
2023-05-12 23:01:49 -0400

    log in page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 
 
 import ReactDOM from "react-dom/client";
+import './App.css';
+const App = (props) => {
 
+  return (
+    <>
+      <div>
+        
+      </div>
+      <div class="starter" >Hello World.</div>
+      <div class="navigation-bar">
+
+      </div>
+
+      <div class="login-form">
+        <input type="text" placeholder="Enter Username" required></input>
+        <input style={{marginTop:"5%"}}type="text" placeholder="Enter Passweord" required></input>
+
+        <button onClick={New} type="submit">Login</button>
+
+
+      </div>
+    </>
+
+  )
+}
 const New = (props) =>{
   return(
     <>
@@ -16,4 +40,4 @@ const New = (props) =>{
   )
 }
 
-
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from 'react'
 import './App.css';
 import ReactDOM from 'react-dom/client'
+import App from './App';
 
 
-const Index = (props) => {
-
-    return (
-      <>
-        <div>
-          
-        </div>
-        <div class="starter" >Hello World.</div>
-        <div class="navigation-bar">
-  
-        </div>
-  
-        <div class="login-form">
-          <input type="text" placeholder="Enter Username" required></input>
-          <input style={{marginTop:"5%"}}type="text" placeholder="Enter Password" required></input>
-  
-          <button type="submit">Login</button>
-  
-  
-        </div>
-      </>
-  
-    )
-  }
-ReactDOM.createRoot(document.getElementById('root')).render(<Index />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
