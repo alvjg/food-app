dc5095f52d2bc14ff9e934a96a1a879a903758ca
Alvin Jiang <alvinjg@umich.edu>
2023-07-25 22:07:53 -0400

    Implemeneted settings icon
    Replaced Settings text in navbar with an icon. Icon is vertically center but not be the best way to do it.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom"
+import { FaGear } from "react-icons/fa6";
 import './NavBar.css'
 
 const NavBar = () => {
@@ -8,11 +9,11 @@ const NavBar = () => {
             <nav class="navbar">
 
                 {/* NavLink has conditional class="active" (only appears when user clicks on the link)*/}
-                <NavLink class="logo" style={{}} to="/">Foodie Finder</NavLink>
+                <NavLink className="logo" to="/">Foodie Finder</NavLink>
                 <NavLink to="/restaurants">Restaurants</NavLink>
-                <NavLink class="setting" to="/settings"></NavLink>
                 <NavLink to="/help">Help</NavLink>
                 <NavLink to="/map">Map</NavLink>
+                <NavLink className="setting" title="Settings" alt="Settings"  to="/settings"><FaGear id="settings-icon"/></NavLink>
             </nav>
 
         </>)
